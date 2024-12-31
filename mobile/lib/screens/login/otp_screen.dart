fe49309ca9d6bbf98020cb3dba8c0adefe3fdd88
Nam Tran <tranducnhatnam27@gmail.com.vn>
2024-12-31 10:39:10 +0700

    Refactor: Improve OTP screen and update translations
    This commit refactors the OTP screen and updates translations for English and Vietnamese.
    
    -   **OTP Screen:**
        -   Add `TextEditingController` for OTP input.
        -   Update `Pinput` widget to show obscure text
        -   Refactor `Pinput` to set text and clear when error.
        -   Show loading animation during OTP submission.
    -   **Translations:**
        - Update `title` and `subtitle` for `logout`, `exit` and `sessionEnded`
        - Update `subtitle` for `otp`
    -   **Remove:**
        - Remove unused `logout` in translation.
        - Remove `sessionEnded` in translation.

--- mobile/lib/screens/login/otp_screen.dart
+++ mobile/lib/screens/login/otp_screen.dart
@@ -2,14 +2,18 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
+import 'package:loading_animation_widget/loading_animation_widget.dart';
 import 'package:mobile/bloc/signup/otp/otp_cubit.dart';
 import 'package:mobile/widgets/app_public_screen.dart';
+import 'package:mobile/widgets/toast/dialog.dart';
 import 'package:pinput/pinput.dart';
 
 import '../../widgets/toast/toast.dart';
 
 class OtpScreen extends StatelessWidget {
-  const OtpScreen({super.key});
+  OtpScreen({super.key});
+
+  final TextEditingController controller = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -26,35 +30,50 @@ class OtpScreen extends StatelessWidget {
                 text: context.tr("error"),
                 description: message,
               );
-
             },
           );
         },
         child: BlocBuilder<OtpCubit, OtpState>(
           builder: (context, state) => AppPublicScreen(
             formChildren: [
-              Pinput(
-                length: 6,
-                defaultPinTheme: PinTheme(
-                  width: 56,
-                  height: 56,
-                  textStyle: TextStyle(
-                    fontSize: 20,
-                    color: Theme.of(context).colorScheme.onPrimary,
-                    fontWeight: FontWeight.w600,
+              state.maybeWhen(
+                loaded: (username) => Pinput(
+                  controller: controller,
+                  length: 6,
+                  defaultPinTheme: PinTheme(
+                    width: 56,
+                    height: 56,
+                    textStyle: TextStyle(
+                      fontSize: 20,
+                      color: Theme.of(context).colorScheme.onPrimary,
+                      fontWeight: FontWeight.w600,
+                    ),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
                   ),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20),
-                  ),
+                  autofocus: true,
+                  obscureText: true,
+                  onClipboardFound: (value) => controller.setText(value),
+                  onCompleted: (pin) {
+                    context.read<OtpCubit>().submit(pin).onError(
+                      (error, stackTrace) {
+                        controller.clear();
+                      },
+                    );
+                  },
                 ),
-                autofocus: true,
-                onCompleted: (pin) => context.read<OtpCubit>().submit(pin),
+                loading: () => LoadingAnimationWidget.staggeredDotsWave(
+                  color: Theme.of(context).colorScheme.primary,
+                  size: 56,
+                ),
+                orElse: () => const SizedBox(),
               )
             ],
             onPressed: null,
             isNoButton: true,
             title: context.tr("title.otp"),
-            subTitle: context.tr("subTitle.otp"),
+            subTitle: context.tr("subtitle.otp"),
           ),
         ),
       ),
